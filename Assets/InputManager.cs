940e379fea309517b88f523ac4fe13b6918aae43
TrHuongIT <tramhuongptit@gmail.com>
2023-04-13 11:01:06 +0700

    E5 - SAI_GAME

---
+++ Assets/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager instance;
+
+    public static InputManager GetInstance { get => instance; }
+
+    [SerializeField] private Vector3 mouseWorldPos;
+
+    public Vector3 MouseWorldPos { get => mouseWorldPos; }
+
+    private void Awake()
+    {
+        if (instance != null) Debug.LogError("Chỉ có 1 InputManager!");
+
+        instance = this;
+    }
+
+    private void FixedUpdate()
+    {
+        GetMousePos();
+
+    }
+
+    protected virtual void GetMousePos()
+    {
+        mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
